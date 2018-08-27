3a318f660d8973bd26b646f52a982b228219e428
Scott McCutchen <smccutchen@users.noreply.github.com>
2018-08-27 01:03:59 -0400

    Consulting page looks good

--- soverance.com/Controllers/HomeController.cs
+++ soverance.com/Controllers/HomeController.cs
@@ -66,7 +66,7 @@ namespace soverance.com.Controllers
             return View();
         }
 
-        public IActionResult Pricing()
+        public IActionResult Consulting()
         {
             return View();
         }
