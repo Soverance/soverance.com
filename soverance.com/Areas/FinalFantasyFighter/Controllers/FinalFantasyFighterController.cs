c9e70940f335157241c99d4eba649d37bb14fdeb
Scott McCutchen <smccutchen@users.noreply.github.com>
2018-09-03 15:31:57 -0400

    Added FFXI: Fighter portfolio page

---
+++ soverance.com/Areas/FinalFantasyFighter/Controllers/FinalFantasyFighterController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authorization;
+using soverance.com.Models;
+
+namespace soverance.com.FinalFantasyFighter.Controllers
+{
+    [Area("FinalFantasyFighter")]
+    public class FinalFantasyFighterController : Controller
+    {
+        [Route("ffxi-fighter")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
