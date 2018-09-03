f4dfc7414adf51f5b8e1f6f1b9ae79a8dc92da0c
Scott McCutchen <smccutchen@users.noreply.github.com>
2018-09-03 18:36:48 -0400

    Added CTF-Elemental Area

--- soverance.com/Areas/Elemental/Controllers/ElementalController.cs
+++ soverance.com/Areas/Elemental/Controllers/ElementalController.cs
@@ -11,28 +11,28 @@ using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Authorization;
 using soverance.com.Models;
 
-namespace soverance.com.Echopshere.Controllers
+namespace soverance.com.Elemental.Controllers
 {
     [Authorize]
-    [Area("Echosphere")]
-    public class EchosphereController : Controller
+    [Area("Elemental")]
+    public class ElementalController : Controller
     {
         private readonly IOptions<SecretConfig> SovSecretConfig;
 
-        public EchosphereController(IOptions<SecretConfig> _SovSecretConfig)
+        public ElementalController(IOptions<SecretConfig> _SovSecretConfig)
         {
             // construct the configuration objects for this controller
             SovSecretConfig = _SovSecretConfig;
         }
 
         [AllowAnonymous]
-        [Route("echosphere")]
+        [Route("ctf-elemental")]
         public IActionResult Index()
         {
             string GoogleApiKey = SovSecretConfig.Value.GoogleApiKey;
-            List<YouTubeData> EchosphereList = YouTubeModel.GetVideos(GoogleApiKey, false, "PLXZQqd9R-mFEHtjtYdgI0OtAcIn1Ta_4p");
-            ViewBag.EchospherePlaylist = EchosphereList.Take(5);
-            ViewBag.ShowcaseVideo = EchosphereList[0];
+            List<YouTubeData> ElementalList = YouTubeModel.GetVideos(GoogleApiKey, false, "PLXZQqd9R-mFFvBMV2lVfe4vixRQKqrVUO");
+            ViewBag.ElementalPlaylist = ElementalList.Take(5);
+            ViewBag.ShowcaseVideo = ElementalList[0];
 
             return View();
         }
