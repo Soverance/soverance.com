4f88e61d83fc5c78ba86b07a3575113235a97387
Scott McCutchen <smccutchen@users.noreply.github.com>
2018-09-03 21:40:58 -0400

    Added Endless Reach VR portfolio item
    Also fixed a spacing issue with youtube feeds on certain portfolio pages

--- soverance.com/Areas/EndlessReach/Controllers/EndlessReachController.cs
+++ soverance.com/Areas/EndlessReach/Controllers/EndlessReachController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using soverance.com.Models;
 
 namespace soverance.com.EndlessReach.Controllers
@@ -16,10 +17,30 @@ namespace soverance.com.EndlessReach.Controllers
     [Area("EndlessReach")]
     public class EndlessReachController : Controller
     {
+        private readonly IOptions<SecretConfig> SovSecretConfig;
+
+        public EndlessReachController(IOptions<SecretConfig> _SovSecretConfig)
+        {
+            // construct the configuration objects for this controller
+            SovSecretConfig = _SovSecretConfig;
+        }
+
         [AllowAnonymous]
         [Route("endlessreach")]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [Route("endlessreach/vr")]
+        public IActionResult VR()
+        {
+            string GoogleApiKey = SovSecretConfig.Value.GoogleApiKey;
+            List<YouTubeData> ERVRList = YouTubeModel.GetVideos(GoogleApiKey, false, "PLXZQqd9R-mFHdg05pfPQ_c3Mp-QJ6-pBz");
+            ViewBag.ERVRPlaylist = ERVRList.Take(6);
+            ViewBag.ShowcaseVideo = ERVRList[0];
+
             return View();
         }
     }
