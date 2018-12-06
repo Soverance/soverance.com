be07d0a607682100ae9b6d06adef7fa7a4ba9bb0
Scott McCutchen <smccutchen@users.noreply.github.com>
2018-12-05 21:57:37 -0500

    Job listings

---
+++ soverance.com/Areas/Jobs/Controllers/JobsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Logging;
+using soverance.com.Models;
+
+namespace soverance.com.Jobs.Controllers
+{
+    [Authorize]
+    [Area("Jobs")]
+    public class JobsController : Controller
+    {
+        [AllowAnonymous]
+        [Route("jobs")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [Route("jobs/combat-engineer")]
+        public IActionResult CombatEngineer()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [Route("jobs/level-designer")]
+        public IActionResult LevelDesigner()
+        {
+            return View();
+        }
+    }
+}
